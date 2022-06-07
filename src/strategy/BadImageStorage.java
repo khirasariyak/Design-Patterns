a3912bfbd02e61ae05ecabd68a5655ff405879cb
Kishan <kishankhirasariya.kk@gmail.com>
2022-06-07 21:55:38 +0530

    Added the strategy design pattern

---
+++ src/strategy/BadImageStorage.java
@@ -0,0 +1,23 @@
+package strategy;
+
+public class BadImageStorage {
+    private String compressor;
+    private String filter;
+
+    public BadImageStorage(String compressor, String filter) {
+        this.compressor = compressor;
+        this.filter = filter;
+    }
+
+    public void store(String fileName){
+        if (compressor == "jpeg")
+            System.out.println("Compressed using JPEG");
+        else if (compressor == "png")
+            System.out.println("Compressed using PNG");
+
+        if (filter == "b&w")
+            System.out.println("Applying B&W filter");
+        else if (filter == "high-contrast")
+            System.out.println("Applying high-contrast filter");
+    }
+}
---
+++ src/strategy/Main.java
@@ -0,0 +1,9 @@
+package strategy;
+
+public class Main {
+    public static void main(String[] args) {
+        var imageStorage = new ImageStorage();
+        imageStorage.store("a", new JpegCompressor(), new BlackAndWhiteFilter());
+        imageStorage.store("b", new PngCompressor(), new MonochromeFilter());
+    }
+}
